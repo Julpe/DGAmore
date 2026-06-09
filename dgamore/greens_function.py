Added calculation of kinetic and potential energy and fixed index flip in local diagrams for Eliashberg equation.

dgamore/greens_function.py
@@ -106,16 +106,6 @@ class GreensFunction(IAmNonLocal, LocalNPoint):
             config.sys.n, config.sys.occ, config.sys.occ_k = self.get_fill_nonlocal()
 
     @property
-    def e_kin(self):
-        """
-        Returns the kinetic energy of the system, see Eq (22) in G. Rohringer & A. Toschi
-        PHYSICAL REVIEW B 94, 125144 (2016).
-        """
-        ekin = 2 / config.sys.beta * np.sum(np.mean(self._ek[..., None] * self.mat, axis=(0, 1, 2)))
-        assert np.abs(ekin.imag) < 1e-8, "Kinetic energy must be real."
-        return ekin.real
-
-    @property
     def ek(self) -> np.ndarray:
         """
         Returns the band dispersion as a numpy array.
@@ -198,7 +188,7 @@ class GreensFunction(IAmNonLocal, LocalNPoint):
 
     def transpose_orbitals(self):
         r"""
-        Transposes the orbitals of the Green's function object like :math:`G_{ab}^k -> G_{ba}^k.
+        Transposes the orbitals of the Green's function object like :math:`G_{ab}^k -> G_{ba}^k`.
         """
         return self.permute_orbitals("ab->ba")
 
@@ -238,6 +228,26 @@ class GreensFunction(IAmNonLocal, LocalNPoint):
         occ_mean.real[np.abs(occ_mean) < 1e-12] = 0.0
         n_el = 2.0 * np.trace(occ_mean).real
         return n_el, occ_mean, occ_k
+
+    def get_ekin_total(self) -> float:
+        r"""
+        Returns the kinetic energy calculated from the Green's function and the k-dependent occupation.
+        :math:`E_{kin} = \sum_{\sigma\vec{k}ab} \varepsilon(\vec{k})_{ab} n(\vec{k})_{ba}`.
+        """
+        return 2 * np.sum(self._ek * config.sys.occ_k.swapaxes(-1, -2)).real / config.lattice.k_grid.nk_tot
+
+    def get_epot_total(self) -> float:
+        r"""
+        Returns the potential energy calculated from the Green's function and the self-energy.
+        :math:`E_{pot} = 1/(2\beta) \sum_{\sigma\vec{k}\nu ab} \Sigma(\vec{k},\nu)_{ab} G(\vec{k},\nu)_{ba}`.
+        """
+        return (
+            (self._sigma.decompress_q_dimension().mat * self.decompress_q_dimension().transpose_orbitals().mat)
+            .sum()
+            .real
+            / config.sys.beta
+            / config.lattice.k_grid.nk_tot
+        )
 
     def _get_gfull_mat(self) -> np.ndarray:
         """

dgamore/nonlocal_sde.py
@@ -760,7 +760,7 @@ def calculate_self_energy_q(
         config.sys.n, config.sys.occ, config.sys.occ_k = giwk_full.get_fill_nonlocal()
         giwk_full.cut_niv(niv_cut)
 
-        if sigma_old is sigma_dmft:
+        if np.allclose(sigma_old.cut_niv(config.box.niv_core).mat, sigma_dmft.cut_niv(config.box.niv_core).mat):
             giwk_full.save(output_dir=config.output.output_path, name="g_latt_dmft")
     config.sys.n, config.sys.occ, config.sys.occ_k = comm.bcast(
         (config.sys.n, config.sys.occ, config.sys.occ_k), root=0
@@ -774,7 +774,13 @@ def calculate_self_energy_q(
     v_nonloc_full = deepcopy(v_nonloc)
     v_nonloc = v_nonloc.reduce_q(my_irr_q_list)
 
+    # old_mixing_history_length = config.self_consistency.mixing_history_length
+    # config.self_consistency.mixing_history_length = -1
+
     for current_iter in range(starting_iter + 1, starting_iter + config.self_consistency.max_iter + 1):
+        # if config.self_consistency.mixing_history_length < old_mixing_history_length:
+        # config.self_consistency.mixing_history_length += 1
+
         logger.info("----------------------------------------")
         logger.info(f"Starting iteration {current_iter}.")
         logger.info("----------------------------------------")
@@ -916,9 +922,17 @@ def calculate_self_energy_q(
             # calculate new occupation matrix from new Green's function (outside asympt region it is the DMFT
             # lattice Green's function)
             _, config.sys.occ, config.sys.occ_k = giwk_occ.get_fill_nonlocal()  # n should not change
+
+            ekin = giwk_occ.get_ekin_total()
+            logger.info(f"Kinetic energy: {ekin:.4f} [t or eV].")
+
+            epot = giwk_occ.get_epot_total()
+            logger.info(f"Potential energy: {epot:.4f} [t or eV].")
+            logger.info(f"Total energy: {(ekin + epot):.4f} [t or eV].")
         config.sys.occ, config.sys.occ_k = comm.bcast((config.sys.occ, config.sys.occ_k), root=0)
+
         if config.self_consistency.max_iter > 1:
-            logger.info("Updated occupation matrix with new Green's function.")
+            logger.info("Updated occupation matrix from new Green's function.")
 
         if comm.rank == 0:
             sigma_new.save(name=f"sigma_dga_iteration_{current_iter}", output_dir=config.output.output_path)